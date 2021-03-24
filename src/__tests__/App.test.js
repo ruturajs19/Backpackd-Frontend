8301fcbf9200f003ecdd52f0e9bace3b7b63510e
Ruturaj Swain <Swain_Ruturaj@cat.com>
2021-03-24 16:38:46 +0530

    Adding Google Analytics and improvements

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { configure, shallow } from "enzyme";
+import { configure, mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import App from "../App";
+import Users from "../user/pages/Users";
+import UserPlaces from "../places/pages/UserPlaces";
+import Auth from "../user/pages/Auth";
 
 configure({ adapter: new Adapter() });
 
 describe("App", () => {
-  it("Render App", () => {
+  it("Should render App Component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Route")).toHaveLength(3);
     expect(wrapper.find("Route").at(0).props().path).toEqual("/");
     expect(wrapper.find("Route").at(1).props().path).toEqual("/:userId/places");
     expect(wrapper.find("Route").at(2).props().path).toEqual("/auth");
+  });
+  it("Should render Users Component for default route", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Route[path='/']").contains(<Users />)).toBe(true);
+  });
+  it("Should render UserPlaces Component for /:userId/places route", () => {
+    const wrapper = shallow(<App />);
+    expect(
+      wrapper.find("Route[path='/:userId/places']").contains(<UserPlaces />)
+    ).toBe(true);
+  });
+  it("Should render Auth Component for /auth route", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Route[path='/auth']").contains(<Auth />)).toBe(true);
   });
 });
